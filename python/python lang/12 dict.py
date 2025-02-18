5bd0c7e072406e96209dd7ae19d62a011d65f261
sabbircse499 <sabbir472003@gmail.com>
2025-02-19 00:59:21 +0600

    ifelse and loop

---
+++ python/python lang/12 dict.py
@@ -0,0 +1,12 @@
+thisdist={
+
+"name":"sabbir",
+"age":"21",
+"cgpa":"3.14"
+}
+
+
+print(thisdist)
+print(thisdist["name"])
+
+
---
+++ python/python lang/13 if else.py
@@ -0,0 +1,39 @@
+# if condotion 
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+# elif
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+# elif else
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+# Short Hand If
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+
+
+
+
+
