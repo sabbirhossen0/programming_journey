9d541d59d92fc033f1d3270420425395bf386763
sabbircse499 <sabbir472003@gmail.com>
2025-02-20 23:59:12 +0600

    iter

---
+++ python/python lang/19 iteration.py
@@ -0,0 +1,17 @@
+# char access using iter and next
+mystr='sabbir'
+newstr=iter(mystr)
+
+print(next(newstr))
+print(next(newstr))
+print(next(newstr))
+print(next(newstr))
+print(next(newstr))
+print(next(newstr))
+
+# list access  using iter and next
+mylist=["sabbir","sajjad","rimon"]
+newlist=iter(mylist)
+print(next(newlist))
+print(next(newlist))
+print(next(newlist))
