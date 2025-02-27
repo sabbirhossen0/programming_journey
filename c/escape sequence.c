0c1d55d35c09d062818d2e7522600dae522cb49e
sabbircse499 <sabbir472003@gmail.com>
2025-02-27 13:40:48 +0600

    web

---
+++ c/escape sequence.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+printf("\t \" \\sabbir \n%f",12.30);
+
+//swaping
+int kasem=10;
+int hasem=20;
+
+int temp =kasem; //10
+
+kasem=hasem; //20
+hasem=temp; //10
+printf("%d \t %d",kasem,hasem);
+
+int _rafi_701=10;
+
+
+
+
+}
