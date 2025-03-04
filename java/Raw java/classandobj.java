d9252ab0c9c9648cfbd68450ab408b10806551d9
sabbircse499 <sabbir472003@gmail.com>
2025-03-05 05:40:19 +0600

    java

--- java/Raw java/classandobj.java
+++ java/Raw java/classandobj.java
@@ -1,8 +1,28 @@
+class student{
+String name;
+int id;
+String section;
+    student(){
+name="sasbbir";
+id =499;
+section="c";
+    }
+
+}
+
+
 public class classandobj{
 
 public static void main(String[] args){
-    
-System.out.println(" hello java");
+
+System.out.println(" hello java\n");
+
+classandobj ob=new classandobj();
+
+System.out.println();
+
+
+
 
 }
 
