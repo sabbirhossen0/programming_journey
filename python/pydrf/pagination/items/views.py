dce3ec274df2fc72c9780a774cddc49230941e07
sabbircse499 <sabbir472003@gmail.com>
2025-03-06 23:36:09 +0600

    drf

--- python/pydrf/pagination/items/views.py
+++ python/pydrf/pagination/items/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Item
+from .serializers import ItemSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def paginated_items(request):
+    queryset = Item.objects.all().order_by('id')
+    paginator = PageNumberPagination()
+    paginator.page_size = 10  # Items per page
+    result_page = paginator.paginate_queryset(queryset, request)
+    return paginator.get_paginated_response(ItemSerializer(result_page, many=True).data)
