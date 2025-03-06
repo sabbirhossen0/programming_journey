55e296e61199ceb7eeb575e56ae6729545728065
sabbircse499 <sabbir472003@gmail.com>
2025-03-06 23:21:28 +0600

    drf

--- java/Raw java/classandobj.java
+++ java/Raw java/classandobj.java
@@ -7,23 +7,13 @@ name="sasbbir";
 id =499;
 section="c";
     }
-
 }
-
-
 public class classandobj{
-
 public static void main(String[] args){
-
 System.out.println(" hello java\n");
-
-classandobj ob=new classandobj();
-
-System.out.println();
-
-
-
-
+student ob=new student();
+int id1=ob.id;
+System.out.println(id1);
 }
 
 }
