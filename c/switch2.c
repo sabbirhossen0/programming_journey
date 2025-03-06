ed750c7a7114b6b177a865c56baaa79d0105051b
sabbircse499 <sabbir472003@gmail.com>
2025-03-07 00:39:57 +0600

    c

--- c/switch2.c
+++ c/switch2.c
@@ -1,38 +1,43 @@
 #include <stdio.h>
 int main(){
 
-    // a
-    // +
-    // b
-    // ans:a+b
-
-    // 10
-    // +
-    // 10
-    // result=20
-
 float number1;
 float number2;
-char operation;
-
+char operation;//+
+printf("Enter number 1 :");
 scanf("%f",&number1);
-scanf(" %c",&operation);
+printf("Tell Which operation you want  :");
+scanf(" %c",&operation); //-+*/
+printf("Enter number 2 :");
 scanf("%f",&number2);
 
 
 switch (operation)
 {
-case '+' :
+    case '+' :   //operation=='+'
     printf("\n Sum :%f",number1+number2);
     break;
+
 
     case '-' :
     printf("\n subtruction : %f",number1-number2);
     break;
 
+    case '*' :
+    printf("\n multiplication : %f",number1*number2);
+    break;
+    case '/' :
+    printf("\nDivision : %f",number1/number2);
+    break;
 
 default:
     break;
 }
+
+
+
+
+
+
 
 }
