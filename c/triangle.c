f517525126acc1c766055a1f32babb6fdb229e4a
sabbircse499 <sabbir472003@gmail.com>
2025-03-12 01:13:07 +0600

    hello

--- c/triangle.c
+++ c/triangle.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 int main(){
 
-    
+    float a,b,c;
+    scanf("%f",&a);
+    scanf("%f",&b);
+    scanf("%f",&c);
+
+    float result=a+b+c;
+
+    if(result == 180){
+        printf("yes its triangle");
+    }
+    else{
+        printf("its not triengle");
+    }
 
 }
---
+++ java/Raw java/basic.java
@@ -0,0 +1,12 @@
+
+
+
+
+public class basic{
+
+    public static void main(String[] args){
+        System.out.println("hello this is sabbir hossen ");
+    }
+
+
+}
