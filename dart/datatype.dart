5e1392d0df426d5079fc23ba99269e9ec55b3014
sabbircse499 <sabbir472003@gmail.com>
2025-03-17 23:42:44 +0600

    dart

---
+++ dart/datatype.dart
@@ -0,0 +1,16 @@
+void main() {
+  int id = 1;
+  double rating = 4.5;
+  String name = 'Flutter';
+  bool isAvailable = true;
+  
+  List<String> skills = ['Dart', 'Flutter'];
+  Map<String, int> scores = {'Math': 90, 'English': 85};
+  
+  print('ID: $id');
+  print('Rating: $rating');
+  print('Name: $name');
+  print('Available: $isAvailable');
+  print('Skills: $skills');
+  print('Scores: $scores');
+}
