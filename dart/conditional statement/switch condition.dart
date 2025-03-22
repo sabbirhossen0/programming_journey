512dc225d5f6d65bfa184234fb6387da1f951620
sabbircse499 <sabbir472003@gmail.com>
2025-03-22 22:16:50 +0600

    switch

---
+++ dart/conditional statement/switch condition.dart
@@ -0,0 +1,17 @@
+void main() {
+  String grade = 'B';
+
+  switch (grade) {
+    case 'A':
+      print('Excellent!');
+      break;
+    case 'B':
+      print('Very Good!');
+      break;
+    case 'C':
+      print('Good!');
+      break;
+    default:
+      print('Invalid grade!');
+  }
+}
