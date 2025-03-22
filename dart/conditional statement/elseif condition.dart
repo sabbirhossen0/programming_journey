632c4e71cc00047841de172391479316b2896429
sabbircse499 <sabbir472003@gmail.com>
2025-03-22 22:15:22 +0600

    dart

---
+++ dart/conditional statement/elseif condition.dart
@@ -0,0 +1,13 @@
+void main() {
+  int marks = 75;
+
+  if (marks >= 90) {
+    print('Grade A');
+  } else if (marks >= 80) {
+    print('Grade B');
+  } else if (marks >= 70) {
+    print('Grade C');
+  } else {
+    print('Grade F');
+  }
+}
