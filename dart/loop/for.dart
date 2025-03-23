1385538047ba262fe9364d8fcb68d8680b130529
sabbircse499 <sabbir472003@gmail.com>
2025-03-24 01:27:47 +0600

    for loop

--- dart/loop/for.dart
+++ dart/loop/for.dart
@@ -2,4 +2,13 @@ void main() {
   for (int i = 0; i < 5; i++) {
     print('Count: $i');
   }
+  
+List<String> fruits = ['Apple', 'Banana', 'Cherry'];
+
+  for (int i = 0; i < fruits.length; i++) {
+    print('Fruit ${i + 1}: ${fruits[i]}');
+  }
+
+
+
 }
