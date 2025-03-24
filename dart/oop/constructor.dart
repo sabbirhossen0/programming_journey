76850e40b89e820d0f2e78adadbfc170746e63c1
sabbircse499 <sabbir472003@gmail.com>
2025-03-24 23:14:33 +0600

    dart

---
+++ dart/oop/constructor.dart
@@ -0,0 +1,19 @@
+class Car {
+  String brand;
+  int year;
+
+  // Constructor
+  Car(this.brand, this.year);
+
+  void displayInfo() {
+    print('Brand: $brand, Year: $year');
+  }
+}
+
+void main() {
+  Car car1 = Car('Toyota', 2020);
+  car1.displayInfo();
+
+  Car car2 = Car('Tesla', 2023);
+  car2.displayInfo();
+}
