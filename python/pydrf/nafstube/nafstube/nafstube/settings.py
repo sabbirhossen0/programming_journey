bc6c03b3846f31136a9c702b37413744af1e5f4d
sabbircse499 <sabbir472003@gmail.com>
2025-04-12 12:02:39 +0600

    main

--- python/pydrf/nafstube/nafstube/nafstube/settings.py
+++ python/pydrf/nafstube/nafstube/nafstube/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-sqw=i4!7dv4s^i$3i-2^higlyp3)p%%&6i$^d6%gqd@e3j9r%4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.100','127.0.0.1']
+ALLOWED_HOSTS = ['192.168.0.101','127.0.0.1']
 
 
 # Application definition
---
+++ python/pythonlang/filehandling/read.py
@@ -0,0 +1,2 @@
+f = open("pythona.txt", "r")
+print(f.read())
