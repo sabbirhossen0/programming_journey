4ef04984d3d69a3183a169510079c13adc431bc9
sabbircse499 <sabbir472003@gmail.com>
2025-04-15 23:59:04 +0600

    drf

--- python/pydrf/query/que/models.py
+++ python/pydrf/query/que/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class BlogPost(models.Model):
+    STATE_CHOICES = [
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    state = models.CharField(max_length=10, choices=STATE_CHOICES, default='draft')
+    read_count = models.IntegerField(default=0)
+    reading_time = models.IntegerField(help_text="Estimated reading time in minutes")
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
