2f8b5e9749080a2a1b3d061f20d7aa77fab6c37b
sabbircse499 <sabbir472003@gmail.com>
2025-04-19 01:25:26 +0600

    drf

--- python/pydrf/peartsoft/contact/views.py
+++ python/pydrf/peartsoft/contact/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+# contact_app/views.py
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.core.mail import send_mail
+from .models import Contact
+from .serializers import ContactSerializer
 
-# Create your views here.
+@api_view(['POST'])
+def contact_us(request):
+    serializer = ContactSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+        # Send email to you
+        subject = "New Contact Form Submission"
+        message = f"""
+        Name: {request.data.get('first_name')} {request.data.get('last_name')}
+        Email: {request.data.get('email')}
+        Message: {request.data.get('message')}
+        """
+        send_mail(
+            subject,
+            message,
+            'your_email@gmail.com',  # Change to your Gmail
+            ['sabbir@gmail.com'],    # Your email where the message goes
+        )
+
+        return Response({"status": "Message sent and saved successfully"})
+    return Response(serializer.errors)
