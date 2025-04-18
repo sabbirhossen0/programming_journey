2b370a9ece3f04948dc5b721ad0d5874f2df8c28
sabbircse499 <sabbir472003@gmail.com>
2025-04-19 01:08:29 +0600

    der

--- python/pydrf/peartsoft/contact/models.py
+++ python/pydrf/peartsoft/contact/models.py
@@ -1,3 +1,11 @@
+# contact_app/models.py
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.email
