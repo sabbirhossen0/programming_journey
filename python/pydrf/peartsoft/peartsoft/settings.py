07ae7e3b182a113c315d1eab4e82eace7111cc3c
sabbircse499 <sabbir472003@gmail.com>
2025-04-19 19:00:19 +0600

    drf

--- python/pydrf/peartsoft/peartsoft/settings.py
+++ python/pydrf/peartsoft/peartsoft/settings.py
@@ -145,5 +145,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'wonderfully701@gmail.com'  # Use your Gmail address
 EMAIL_HOST_PASSWORD = 'tbwx ukgl uamb tdda'  # Use the app password (not your real password)
