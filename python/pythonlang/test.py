4f97eb224ccb32eaf1fcc8da9e2cfe688a63517c
sabbircse499 <sabbir472003@gmail.com>
2025-04-22 00:31:05 +0600

    drf

---
+++ python/pythonlang/test.py
@@ -0,0 +1,11 @@
+a = int(input("Enter your age: "))
+print("your age is:", a)
+print(a>18)
+print(a<18)
+print(a>=18)
+print(a<=18)
+print(a==18)
+if(a>18):
+    print("you can drive")
+else:
+    print("you cannot drive")
