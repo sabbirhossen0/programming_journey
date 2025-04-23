b726be8cbad021ba703b9221d1515fcb4e80b6c3
sabbircse499 <sabbir472003@gmail.com>
2025-04-23 23:52:50 +0600

    nested model

--- python/pydrf/nestedserializer/nested/models.py
+++ python/pydrf/nestedserializer/nested/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class book(models.Model):
+    name=models.CharField(max_length=100)
+    author=models.CharField(max_length=100)
+    price=models.IntegerField()
+    
+
+    def __str__(self):
+        return self.name
+
+c
