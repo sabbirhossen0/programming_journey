7624623aa7b3de70c2eab2e94727fce8655e15e1
sabbircse499 <sabbir472003@gmail.com>
2025-04-23 23:55:48 +0600

    drf

--- python/pydrf/nestedserializer/nested/models.py
+++ python/pydrf/nestedserializer/nested/models.py
@@ -10,4 +10,10 @@ class book(models.Model):
     def __str__(self):
         return self.name
 
-c
+class author (models.Model):
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+    books=models.ManyToManyField(book)
+    
+    def __str__(self):
+        return super().__str__()
