159430f1d7f7617740b71e0fdd85e2a9760c3b92
sabbircse499 <sabbir472003@gmail.com>
2025-04-25 23:11:33 +0600

    drf

--- python/pydrf/nestedserializer/nested/serializers.py
+++ python/pydrf/nestedserializer/nested/serializers.py
@@ -7,4 +7,13 @@ class bookSerializer(serializers.ModelSerializer):
     class meta:
         model:book
         field=('name','author','price')
+        def __str__(self):
+            return self.name        
+        
 
+class authorSerializer(serializers.ModelSerializer):
+    class meta:
+        model:author
+        field=('__all__')
+        def  __str__(self):
+            return self.name
