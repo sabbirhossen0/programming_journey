0ad7f10a55d1a6b10929d4c33a272281db3e4889
sabbircse499 <sabbir472003@gmail.com>
2025-04-29 17:27:55 +0600

    drf

--- python/pydrf/nafsshield/nafsshield/settings.py
+++ python/pydrf/nafsshield/nafsshield/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+OPENAI_API_KEY = os.getenv("sk-1234abcd1234abcd1234abcd1234abcd1234abcd")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
