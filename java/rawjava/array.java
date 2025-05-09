57d5fd56ca333ef6673245dbdfaa4a6317a44422
sabbircse499 <sabbir472003@gmail.com>
2025-05-09 17:00:40 +0600

    update

--- java/rawjava/array.java
+++ java/rawjava/array.java
@@ -1,17 +1,13 @@
 import java.util.Scanner;
 
 public class array {
-    public static void main (){
+    public static void main (String[] args){
 
 int[] arr;
         Scanner s=new Scanner(System.in);
 int number =s.nextInt();
 
-
-
-
-
-System.out.print(number);
+System.out.print(number)           ;
 
 s.close();
 
